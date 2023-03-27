1af8c72c8271c1b795448edb1a54dac0c8239ff4
Kenan <kananmh@code.edu.az>
2023-03-27 21:38:37 +0400

    js

---
+++ script.js
@@ -0,0 +1,11 @@
+let answer = prompt('musbet eded yaz gorey');
+
+if(answer>0){
+    console.log('true');
+}
+else if(answer==0){
+    console.log('aa 0 yazma');
+}
+else{
+    console.log('false');
+}
